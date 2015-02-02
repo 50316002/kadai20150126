df334b18284cb120b319943bb2086857f9389b8b
f0316002 <f0316002@ipc.shizuoka.ac.jp>
2015-02-02 14:35:04 +0900

    20150126 submit

---
+++ src/graph_20150126/Graph_main.java
@@ -0,0 +1,11 @@
+package graph_20150126;
+
+public class Graph_main {
+	public static void main (String[] args){
+		Graph_view graph =new Graph_view();
+		graph.setBounds(5,5,655,455);
+		
+		
+	}
+
+}
